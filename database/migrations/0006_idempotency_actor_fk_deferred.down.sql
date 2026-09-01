1e79145e78ef967b0bbd618a2b12c7eac781c209
josem071071-commits <josem071071@gmail.com>
2026-09-01 16:16:18 -0400

    db(cert): add rollback for migration 0006

---
+++ database/migrations/0006_idempotency_actor_fk_deferred.down.sql
@@ -0,0 +1,11 @@
+BEGIN;
+
+ALTER TABLE idempotency_keys
+  DROP CONSTRAINT IF EXISTS idempotency_keys_actor_user_id_fkey;
+
+ALTER TABLE idempotency_keys
+  ADD CONSTRAINT idempotency_keys_actor_user_id_fkey
+  FOREIGN KEY (actor_user_id)
+  REFERENCES users(id);
+
+COMMIT;
